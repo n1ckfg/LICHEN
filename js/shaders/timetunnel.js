128cba0cf836d80c0316d12d35b88c74077c1f27
Nick Fox-Gieg <nick@fox-gieg.com>
2026-02-28 22:03:30 -0500

    Update timetunnel.js

--- js/shaders/timetunnel.js
+++ js/shaders/timetunnel.js
@@ -11,6 +11,13 @@ void main() {
   vec2 uv = zoom * (vTexCoord - 0.5) * vec2(uResolution.x / uResolution.y, 1.0);
   float rInv = 1.0 / length(uv);
   uv = uv * rInv - vec2(rInv + time * speed, 0.5);
-  gl_FragColor = texture2D(tex0, uv) * rInv / 2.0;
+  vec4 col = texture2D(tex0, uv) * rInv / 2.0;
+
+  vec2 flipped = zoom * (vec2(vTexCoord.x, 1.0 - vTexCoord.y) - 0.5) * vec2(uResolution.x / uResolution.y, 1.0);
+  float rInvF = 1.0 / length(flipped);
+  flipped = flipped * rInvF - vec2(rInvF + time * speed, 0.5);
+  vec4 colF = texture2D(tex0, flipped) * rInvF / 2.0;
+
+  gl_FragColor = max(col, colF);
 }
 `;
